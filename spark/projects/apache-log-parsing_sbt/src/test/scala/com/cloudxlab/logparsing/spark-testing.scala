9201936aa2ef63cfe4aa862c7b33e68ab0e72209
Ramesh Jayaraj <rameshjayaraj@users.noreply.github.com>
2020-05-31 00:31:57 +0530

    spark-testing.scala

--- spark/projects/apache-log-parsing_sbt/src/test/scala/com/cloudxlab/logparsing/spark-testing.scala
+++ spark/projects/apache-log-parsing_sbt/src/test/scala/com/cloudxlab/logparsing/spark-testing.scala
@@ -16,8 +16,8 @@ class SampleTest extends FunSuite with SharedSparkContext {
         assert(rdd.count === list.length)   
 
         val records = utils.gettop10(rdd, sc, 10)
-        assert(records.length === 1)    
-        assert(records(0)._1 == "121.242.40.10")
+        assert(records.length === 2)    
+        assert(records(1)._1 == "121.242.40.10")
     }
     test("Should remove IP addresses having 1st Octet Decimal more than 126") {
         var line1 = "121.242.40.10 - - [03/Aug/2015:06:30:52 -0400] \"POST /mod_pagespeed_beacon?url=http%3A%2F%2Fwww.knowbigdata.com%2Fpage%2Fabout-us HTTP/1.1\" 204 206 \"http://www.knowbigdata.com/page/about-us\" \"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0\""
